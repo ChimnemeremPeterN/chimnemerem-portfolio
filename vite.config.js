bde069450b8f54c4ac90d8f139535d9020cbaeae
Chimnemerem Peter Nwala <PeterNwala.work@gmail.com>
2026-09-20 18:39:34 -0700

    Configure Cloudflare deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import oxlint from 'vite-plugin-oxlint'
+import { cloudflare } from '@cloudflare/vite-plugin'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    oxlint(),
+    cloudflare(),
+  ],
 })
